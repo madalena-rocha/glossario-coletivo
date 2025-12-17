ccc92c97df79b9dbee208db1c80b1d8c34b51153
madalena-rocha <rochamada1997m@gmail.com>
2025-12-17 16:31:11 -0300

    Set styles

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export default {
+  COLORS: {
+    VIOLET: "#7129CD",
+    PURPLE_DARK: "#523CB0",
+    PURPLE_LIGHT: "#9B9BFF",
+
+    WHITE: "#FFFFFF",
+    GRAY: "#D9D9D9",
+    BLACK: "#000000",
+  },
+};
