de3146f34b3404d2e42066bc9315a1769c271968
madalena-rocha <rochamada1997m@gmail.com>
2026-01-21 14:50:13 -0300

    Fix button type

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,8 @@
 import { Container } from "./styles";
 
-export function Button({ icon: Icon, title, ...rest }) {
+export function Button({ icon: Icon, title, type = "button", ...rest }) {
   return (
-    <Container type="button" {...rest}>
+    <Container type={type} {...rest}>
       {Icon && <Icon size={19} />}
       {title}
     </Container>
