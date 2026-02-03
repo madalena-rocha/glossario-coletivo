20dd2b413be4ea1c9a0f235e6c231a1fcf11087e
madalena-rocha <rochamada1997m@gmail.com>
2026-02-03 11:36:43 -0300

    Change font

--- src/styles/global.js
+++ src/styles/global.js
@@ -23,7 +23,7 @@ export default createGlobalStyle`
   }
 
   body, input, button, textarea, select {
-	  font-family: 'Roboto', sans-serif;
+	  font-family: 'Poppins', sans-serif;
 	  font-size: 1.6rem;
 	  outline: none;
   }
